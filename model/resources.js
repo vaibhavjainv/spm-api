8c6bae6b8af4d76db5a9b4e24336138d6942f882
Vaibhav Jain <vaibhavj@JainVMBPro-2.local>
2018-04-11 17:56:19 -0400

    Integration updates

--- model/resources.js
+++ model/resources.js
@@ -1,13 +1,22 @@
-var Kitten, Resource;
+var Resource;
 
 var getAllResources = function (res) {
 
     initResources(function () {
-        var resource = new Resource({ name: "Vaibhav", location: "Onshore" })
         Resource.find(function (err, resources) {
             if (err)
                 return console.error(err);
             var data = resources;
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', '*');
+
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+            
             res.status(200).send(data);
         });
     }
@@ -22,6 +31,36 @@ var addResource = function (req, res) {
             if (err) return console.error(err);
             res.status(200).send(resource);
         });
+
+    }
+    );
+}
+
+var deleteResource = function (req, res) {
+
+    initResources(function () {
+
+        Resource.remove({ _id: req.params.id }, function (err) {
+            if (err) return handleError(err);
+            res.status(200).send('Deleted');
+        });
+
+    }
+    );
+}
+
+var updateallocation = function (req, res) {
+
+    initResources(function () {
+
+        Resource.findById(req.body._id, function (err, resource) {
+            if (err) return handleError(err);
+
+            var update = resource.updateallocation(req.body.projects);
+
+            res.status(200).send(update);
+        });
+
 
     }
     );
@@ -52,6 +91,81 @@ var initResources = function (callback) {
                 ]
             });
 
+            resourceSchema.methods.updateallocation = function (projects) {
+
+                if (this.projects.length == 0) {
+                    this.projects = projects;
+                    this.save();
+                } else {
+                    projects.forEach(project => {
+                        var updated = false;
+                        this.projects.forEach(currentproject => {
+                            if (currentproject.account == project.account && currentproject.assignment == project.assignment) {
+                                currentproject.allocation = project.allocation;
+                                updated = true;
+                            }
+                        });
+
+                        if (!updated) {
+                            this.projects.push(project);
+                        }
+                        this.save();
+                    });
+                }
+
+                var sr = {};
+                sr["validation"] = this.validatehours();
+                return (sr);
+            }
+
+            resourceSchema.methods.validatehours = function () {
+                var accumulatedAlloc = [];
+                var result = [];
+
+                this.projects.forEach(project => {
+                    project.allocation.forEach(allocation => {
+
+                        var hours = [];
+                        var currentWeeklyAlloc = {};
+                        var needpush = true;
+
+                        accumulatedAlloc.forEach(weeklyAlloc => {
+                            if (weeklyAlloc[allocation.week.toString()] && needpush) {
+                                currentWeeklyAlloc = weeklyAlloc;
+                                hours = currentWeeklyAlloc[allocation.week.toString()];
+                                needpush = false;
+                            }
+                        });
+
+                        hours[hours.length] = allocation.hours;
+                        currentWeeklyAlloc[allocation.week.toString()] = hours;
+                        if (needpush) {
+                            accumulatedAlloc.push(currentWeeklyAlloc);
+                        }
+
+
+                    });
+                });
+
+                accumulatedAlloc.forEach(weeklyAlloc => {
+
+                    for (const week in weeklyAlloc) {
+                        if (weeklyAlloc.hasOwnProperty(week)) {
+                            const allhours = weeklyAlloc[week];
+                            var totalhours = 0;
+                            allhours.forEach(hours => {
+                                totalhours = totalhours + hours;
+                            });
+                            if (totalhours > 40) {
+                                result[result.length] = totalhours + " allocated for week of " + week;
+                            }
+                        }
+                    }
+                });
+
+                return result;
+            }
+
             Resource = mongoose.model('Resource', resourceSchema);
             callback();
         });
@@ -60,46 +174,9 @@ var initResources = function (callback) {
     }
 }
 
-var getAllKittens = function (res) {
-
-    initKitten(function () {
-        var silence = new Kitten({ name: 'Silence' });
-        Kitten.find(function (err, kittens) {
-            if (err)
-                return console.error(err);
-            var data = kittens;
-            res.status(200).send(data);
-        });
-    }
-    );
-}
-
-var initKitten = function (callback) {
-    if (Kitten == undefined) {
-        var mongoose = require('mongoose');
-        mongoose.connect('mongodb://staffingplan:staffingplan@ds237409.mlab.com:37409/staffing-plan-db');
-        var db = mongoose.connection;
-        db.on('error', console.error.bind(console, 'connection error:'));
-        db.once('open', function () {
-            var kittySchema = mongoose.Schema({
-                name: String
-            });
-            kittySchema.methods.speak = function () {
-                var greeting = this.name
-                    ? "Meow name is " + this.name
-                    : "I don't have a name";
-                console.log(greeting);
-            };
-            Kitten = mongoose.model('Kitten', kittySchema);
-            callback();
-        });
-    } else {
-        callback();
-    }
-}
-
-
 module.exports = {
     getAllResources,
-    addResource
+    addResource,
+    deleteResource,
+    updateallocation
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,22 +2,20 @@ var resources = require("../model/resources.js");
 
 var appRouter = function (app) {
 
-    app.get("/api/getallresources", function (req, res) {
+    app.get("/api/resources/getall", function (req, res) {
         resources.getAllResources(res);
     });
-    
-    app.post("/api/addresource", function (req, res) {
+
+    app.post("/api/resources/add", function (req, res) {
         resources.addResource(req, res);
     });
 
-    app.put("/api/updateresource/:id", function (req, res) {
-        console.log(req.params.id);
-        res.status(200).send();
+    app.put("/api/resources/updateallocation", function (req, res) {
+        resources.updateallocation(req, res);
     });
 
-    app.delete("/api/deleteresource/:id", function (req, res) {
-        console.log(req.params.id);
-        res.status(200).send();
+    app.delete("/api/resources/delete/:id", function (req, res) {
+        resources.deleteResource(req, res);
     });
 }
 
